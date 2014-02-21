b8a8b7a21a006543f330c4f80ca3c33ce4db632a
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-02-21 08:50:46 +0000

    03_01_Creating our first plugins predefined comments to initialise it in wordpress

---
+++ wp-content/plugins/firstPlugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Omatsola Isaac Sobotie's Plugin
+Plugin URI: isobotie.gopagoda.com
+Description: This plugin does things you never thought were possible.
+Author: Omatsola Isaac Sobotie
+Version: 1.0
+Author URI: http://isobotie.gopagoda.com/
+*/
