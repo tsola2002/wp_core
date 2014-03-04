4c91a199db96f004d37f40dbd3e8ddccc29c3d82
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-03-04 10:08:03 +0000

    04_05_foot widget setup with text options

---
+++ wp-content/plugins/Foo_Widget.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * Package: wp_core
+ * Filename: Foo_Widget.php
+ * Author: solidstunna101
+ * Date: 04/03/14
+ * Time: 09:54
+ *
+ */
+
+/*
+Plugin Name: Foo Widget
+Plugin URI: isobotie.gopagoda.com
+Description: This plugin will create a sample widget foo
+Author: Omatsola Isaac Sobotie
+Version: 1.0
+Author URI: http://isobotie.gopagoda.com/
+*/
+
+class Foo_Widget extends WP_Widget {
+
+    /**
+     * Register widget with WordPress.
+     */
+    function __construct() {
+        parent::__construct(
+            'foo_widget', // Base ID
+            __('Widget Title', 'text_domain'), // Name
+            array( 'description' => __( 'A Foo Widget', 'text_domain' ), ) // Args
+        );
+    }
+
+    /**
+     * Front-end display of widget.
+     *
+     * @see WP_Widget::widget()
+     *
+     * @param array $args     Widget arguments.
+     * @param array $instance Saved values from database.
+     */
+    public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+
+        echo $args['before_widget'];
+        if ( ! empty( $title ) )
+            echo $args['before_title'] . $title . $args['after_title'];
+        echo __( 'Hello, World!', 'text_domain' );
+        echo $args['after_widget'];
+    }
+
+
+    public function form( $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        }
+        else {
+            $title = __( 'New title', 'text_domain' );
+        }
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+        </p>
+    <?php
+    }
+
+
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+        return $instance;
+    }
+
+} // class Foo_Widget
+
+// register Foo_Widget widget
+function register_foo_widget() {
+    register_widget( 'Foo_Widget' );
+}
+add_action( 'widgets_init', 'register_foo_widget' );
