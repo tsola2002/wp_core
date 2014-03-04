ff36c414766a163694ff9d5a81a4d07a9cf02360
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-03-04 09:27:52 +0000

    04_04_creating a sample widget for use in wordpress

---
+++ wp-content/plugins/SimpleWidjet.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ * Package: wp_core
+ * Filename: SimpleWidjet.php
+ * Author: solidstunna101
+ * Date: 04/03/14
+ * Time: 09:12
+ *
+ */
+/*
+Plugin Name: Simple Widget
+Plugin URI: isobotie.gopagoda.com
+Description: This plugin will create a sample widget
+Author: Omatsola Isaac Sobotie
+Version: 1.0
+Author URI: http://isobotie.gopagoda.com/
+*/
+
+class SimpleWidget extends WP_Widget
+{
+    function SimpleWidget() {
+
+    }
+
+    function widget() {
+
+    }
+
+    function update() {
+
+    }
+
+    function form() {
+
+    }
+
+}
