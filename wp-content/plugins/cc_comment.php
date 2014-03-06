6458081306b0ec18c35d988664fd6abb5e94bbe0
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-03-06 13:24:30 +0000

    05_01_Creating the admin ui

--- wp-content/plugins/cc_comment.php
+++ wp-content/plugins/cc_comment.php
@@ -8,6 +8,28 @@ Version: 1.0
 Author URI: http://isobotie.gopagoda.com/
 */
 
+
+
+function cccomm_option_page()
+{
+    ?>
+    <div class="wrap">
+        <h2>CC Comments Option Page</h2>
+        <p>Welcome to the CC Comments Plugin. Here you can edit the email(s) you wish to have your comments CC'd to.</p>
+
+    </div>
+<?php
+}
+
+
+function cccomm_plugin_menu()
+{
+    add_options_page('CC Comments Settings','CC Comments', 'manage_options', 'cc-comments-plugin', 'cccomm_option_page');
+}
+
+add_action('admin_menu', 'cccomm_plugin_menu');
+
+
 //function to send email
 function cc_comment()
 {
@@ -18,6 +40,8 @@ function cc_comment()
     $message = "Message from: " . $_REQUEST['name'] . " at email: " . $_REQUEST['email'] . ": \n" . $_REQUEST['comments'];
     mail($to,$subject,$message);
 }
+
+
 
 //add cc_comment to wordpresses exist comment_post action
 add_action('comment_post','cc_comment');
