a6d87cff057e09af9f67d88b68bbbda02cfecd6c
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-03-06 14:32:07 +0000

    05_04_Handling nonces using post worpress 2.7 register settings method

--- wp-content/plugins/cc_comment.php
+++ wp-content/plugins/cc_comment.php
@@ -9,26 +9,27 @@ Author URI: http://isobotie.gopagoda.com/
 */
 
 
+//registering the option settings with wordpress & letting wordpress handle the rest
+function cccomm_init()
+{
+    register_setting('cccomm_options','cccomm_cc_email');
+}
+
+add_action('admin_init','cccomm_init');
 
 function cccomm_option_page()
 {
-    // first...process form data?
-    if ( $_POST['cccomm_hidden'] == 'Y' )
-    {
-        update_option( 'cccomm_cc_email', $_POST['cc_email']);
-        ?>
-        <div id="message" class="updated">Email was saved for CC Comments</div>
-    <?php
-    }
 
     ?>
     <div class="wrap">
         <h2>CC Comments Option Page</h2>
         <p>Welcome to the CC Comments Plugin. Here you can edit the email(s) you wish to have your comments CC'd to.</p>
 
-        <form action="" method="post" id="cc-comments-email-options-form">
-            <h3><label for="cc_email">Email to send CC to: </label> <input
-                    type="text" id="cc_email" name="cc_email"
+        <form action="options.php" method="post" id="cc-comments-email-options-form">
+            <!--  this will output nonces for the entire settings group  -->
+            <?php settings_fields('cccomm_options'); ?>
+            <h3><label for="cccomm_cc_email">Email to send CC to: </label> <input
+                    type="text" id="cccomm_cc_email" name="cccomm_cc_email"
                     value="<?php echo esc_attr( get_option('cccomm_cc_email') ); ?>" /></h3>
             <p><input type="submit" name="submit" value="Update Email" /></p>
             <input type="hidden" name="cccomm_hidden" value="Y" />
