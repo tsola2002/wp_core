01a89be8bc276efcbd006bf66520369760a7db86
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-03-11 12:22:10 +0000

    06_03_accessing more database objects within wordpress database

--- wp-content/plugins/dbinfo.php
+++ wp-content/plugins/dbinfo.php
@@ -16,6 +16,28 @@ function databaseinfo_dashboard_widget()
     <p><b>Last Query: </b> <?php echo $wpdb->last_query ?></p>
     <p><b>Last Error: </b> <?php echo $wpdb->last_error ?></p>
     <p><b>Total Users: </b><?php echo $wpdb->query('SELECT ID FROM wp_users')?></p>
+
+    <p><b>Last post: </b>
+        <?php echo $wpdb->get_var('SELECT post_title FROM ' . $wpdb->posts . ' WHERE post_author = ' . $current_user->ID); ?>
+    </p>
+
+    <p>User Emails:
+    <pre>
+		<?php $user_emails =  $wpdb->get_col('SELECT user_email FROM ' . $wpdb->users);
+        echo var_dump($user_emails); ?>
+	</pre></p>
+
+    <p>Your user info:
+    <pre>
+		<?php $my_user_data = $wpdb->get_row('SELECT * FROM ' . $wpdb->users . ' WHERE ID = ' . $current_user->ID, ARRAY_A);
+        echo var_dump($my_user_data); ?>
+	</pre></p>
+
+    <p><b>All Post Terms: </b>
+    <pre>
+		<?php $terms = $wpdb->get_results('SELECT * FROM ' . $wpdb->terms, ARRAY_N);
+        echo var_dump($terms); ?>
+	</pre></p>
 <?php
 }
 
